6362b0be3510d2a0f663b99a9639e5bf0c39128a
Praful Rana <prafulrana0@gmail.com>
2023-09-30 22:48:57 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
+// Importing required libraries
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Creating an Express Application
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Using body-parser middleware for JSON parsing
 app.use(bodyParser.json());
 
+//Initializing an empty array to store products
 let products = [];
 let getRequestCount = 0;
 let postRequestCount = 0;
 
-// Logging middleware
+// Middleware for logging requestss
 app.use((req, res, next) => {
-  console.log(`> ${req.method} ${req.originalUrl}: received request`);
+  console.log(`> ${req.method} ${req.originalUrl}: Request received`);
   next();
 });
 
-// Define GET endpoint to retrieve images
+// Defining GET endpoint to retrieve products
 app.get('/products', (req, res) => {
-  console.log(`< ${req.method} ${req.originalUrl}: sending response`);
+  console.log(`< ${req.method} ${req.originalUrl}: Sending response`);
   getRequestCount++;
   res.json(products);
 });
 
-// Define POST endpoint to store images
+// Defining POST endpoint to store products
 app.post('/products', (req, res) => {
-  console.log(`< ${req.method} ${req.originalUrl}: received request`);
-  const image = req.body;
-  products.push(image);
+  console.log(`< ${req.method} ${req.originalUrl}: Request received`);
+  const product = req.body;
+  products.push(product);
   postRequestCount++;
-  res.status(201).json({ message: 'Image stored successfully' });
+  res.status(201).json({ message: 'Product stored successfully' });
 });
 
-// Define DELETE endpoint to delete all images
+// Defining DELETE endpoint to delete all products
 app.delete('/products', (req, res) => {
-  console.log(`< ${req.method} ${req.originalUrl}: received request`);
+  console.log(`< ${req.method} ${req.originalUrl}: Request received`);
   products = [];
-  res.json({ message: 'All images deleted' });
+  res.json({ message: 'All products deleted' });
 });
 
-// Start the server
+// Starting the server
 app.listen(PORT, () => {
   console.log(`Server is listening at http://127.0.0.1:${PORT}`);
   console.log('Endpoints:');
   console.log(`http://127.0.0.1:${PORT}/products method: GET, POST`);
 });
 
-// Log request counters on every request
+// Logging request counters on every request
 app.use((req, res, next) => {
-  console.log(`Processed Request Count--> Get: ${getRequestCount}, Post: ${postRequestCount}`);
+  console.log(`Processed Request Count --> GET: ${getRequestCount}, POST: ${postRequestCount}`);
   next();
 });
